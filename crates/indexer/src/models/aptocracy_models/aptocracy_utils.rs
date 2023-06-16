0cd5ed8629bc7da8d44de2ff1ff1c2fda522d009
goran9999 <gutovicgoran@gmail.com>
2023-06-16 11:07:53 +0200

    new devnet module addr

--- crates/indexer/src/models/aptocracy_models/aptocracy_utils.rs
+++ crates/indexer/src/models/aptocracy_models/aptocracy_utils.rs
@@ -156,19 +156,19 @@ impl OrganizationWriteSet {
         data: &serde_json::Value,
     ) -> Result<Option<OrganizationWriteSet>> {
         match data_type {
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::Member<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyMember>" => {
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::Member<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyMember>" => {
                 serde_json::from_value(data.clone())
                 .map(|inner| 
                     Some(OrganizationWriteSet::MemberData(inner))
                 )
             },
-             "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::Proposal<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyProposal>" => {
+             "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::Proposal<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyProposal>" => {
                 serde_json::from_value(data.clone())
                 .map(|inner| 
                     Some(OrganizationWriteSet::ProposalData(inner))
                 )
             },
-             "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::VoteOption" => {
+             "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::VoteOption" => {
                 serde_json::from_value(data.clone())
                 .map(|inner| 
                     Some(OrganizationWriteSet::VoteOptionData(inner))
@@ -220,7 +220,7 @@ impl OrganizationResource {
     fn is_resource_supported(data_type: &str) -> bool {
         matches!(
             data_type,
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::treasury::Treasury" | "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::Organization" | "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::Governances"
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::treasury::Treasury" | "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::Organization" | "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::Governances"
         )
     }
 
@@ -234,13 +234,13 @@ impl OrganizationResource {
         println!("data_type__processor {:?}", data_type);
 
         match data_type {
-         "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::treasury::Treasury" => {
+         "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::treasury::Treasury" => {
                serde_json::from_value(data.clone()).map(|inner| Some(OrganizationResource::CreateTreasury(inner)))
               },
-        "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::Organization" => {
+        "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::Organization" => {
                 serde_json::from_value(data.clone())
                 .map(|inner| Some(OrganizationResource::CreateOrganization(inner)))},
-        "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::Governances" => {
+        "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::Governances" => {
                 serde_json::from_value(data.clone())
                 .map(|inner| Some(OrganizationResource::CreateGovernance(inner)))},
             _ => Ok(None),
@@ -263,7 +263,7 @@ impl AptocracyEvent{
     pub fn from_event(data_type: &str, data: &serde_json::Value) -> Option<Self> {
         println!("DATA_TYPE_test {}",data_type);
         match data_type {
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::VoteEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyProposal>" => {
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::VoteEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyProposal>" => {
                 println!("DATA_test {}",data);
           
                 if let Ok(vote_event)=serde_json::from_value(data.clone()){
@@ -273,7 +273,7 @@ impl AptocracyEvent{
                     None
                 }
             },
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::RelinquishVoteEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyProposal>"=>{
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::RelinquishVoteEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyProposal>"=>{
                 if let Ok(relinquish_vote)=serde_json::from_value(data.clone()){
                     println!("PARSED_data {:?}",relinquish_vote);
                     Some(AptocracyEvent::RelinquishVote(relinquish_vote))
@@ -281,7 +281,7 @@ impl AptocracyEvent{
                     None
                 }
             },
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::CancelProposalEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyProposal>"=>{
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::CancelProposalEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyProposal>"=>{
                 if let Ok(cancel_event)=serde_json::from_value(data.clone()){
                     println!("PARSED_test {:?}",cancel_event);
                     Some(AptocracyEvent::CancelProposal(cancel_event))
@@ -289,7 +289,7 @@ impl AptocracyEvent{
                     None
                 }
             },
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::proposals::FinalizeVoteEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyProposal>"=>{
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::proposals::FinalizeVoteEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyProposal>"=>{
                 if let Ok(finalized_vote)=serde_json::from_value(data.clone()){
                     println!("PARSED_test {:?}",finalized_vote);
                     Some(AptocracyEvent::FinalizeVote(finalized_vote))
@@ -297,21 +297,21 @@ impl AptocracyEvent{
                     None
                 }
             },
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::treasury::DepositEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyTreasury>"=>{
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::treasury::DepositEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyTreasury>"=>{
                 if let Ok(deposit)=serde_json::from_value(data.clone()){
                     Some(AptocracyEvent::Deposit(deposit))
                 }else{
                     None
                 }
             },
-            "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::treasury::WithdrawEvent<0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::aptocracy::AptocracyTreasury>"=>{
+            "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::treasury::WithdrawEvent<0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::aptocracy::AptocracyTreasury>"=>{
                 if let Ok(withdraw)=serde_json::from_value(data.clone()){
                     Some(AptocracyEvent::Withdraw(withdraw))
                 }else{
                     None
                 }
             },
-             "0xa53ed829d1582df3566eff0eefac31b423cbe4b832fffbe25545357db412af10::organization::AcceptMembershipEvent" =>
+             "0x0e38566f0ba66f0b913bd644b8044c7a77528d5ee0a1aede129709ab08b90bc1::organization::AcceptMembershipEvent" =>
              if let Ok(accept_membership) = serde_json::from_value(data.clone()) {
                 Some(AptocracyEvent::AcceptMembershipEvent(accept_membership))
              } else {
